c1f655dc4f599afb0bca2f42e1b95fa842a485d2
Sheraz Ahmed <sherazahmed93@gmail.com>
2022-08-27 15:20:44 +0500

    Added Git Basics to repo

---
+++ react-basics/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import './input.css';
+
+import Login from './components/login';
+
+function App() {
+  return (
+    <div className="App">
+      <Login />      
+    </div>
+  );
+}
+
+export default App;
